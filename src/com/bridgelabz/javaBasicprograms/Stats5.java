f2b2df857df02e6aac93f4e6227423535e5af396
priyankatimgol <48207687+priyankatimgol@users.noreply.github.com>
2019-08-08 22:33:25 +0530

    Add files via upload

---
+++ src/com/bridgelabz/javaBasicprograms/Stats5.java
@@ -0,0 +1,33 @@
+/******************************************************************************
+ 
+ *  Purpose: perform to prints five uniform random values between 0 and 1,
+their average value, and their minimum and maximum value. 
+ *
+ *  @author  PriyankaTimgol
+ *  @version 1.0
+ *  @since   08-08-2017
+ *
+ ******************************************************************************/
+
+
+package com.bridgelabz;
+
+import java.util.Random;
+
+import com.bridgelabz.utility.Utility;
+
+public class Stats5 {
+
+	public static void main(String[] args) {
+		Random random=new Random();
+		double firstnumber=Math.random()*1;
+		double secondnumber=Math.random()*1;
+		double thirdnumber=Math.random()*1;
+		double fourthnumber=Math.random()*1;
+		double fivenumber=Math.random()*1;
+		System.out.println(""+firstnumber+" "+secondnumber+" "+thirdnumber+" "+fourthnumber+" "+fivenumber);
+		Utility.stats5(firstnumber, secondnumber, thirdnumber, fourthnumber, fivenumber);
+
+	}
+
+}
