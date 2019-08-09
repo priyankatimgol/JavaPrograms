0127613e59da4218e9a2233ed384f0c0833a1fb0
priyankatimgol <48207687+priyankatimgol@users.noreply.github.com>
2019-08-09 15:15:19 +0530

    Add files via upload

---
+++ src/com/bridgelabz/javaBasicprograms/CarLoan.java
@@ -0,0 +1,27 @@
+
+/******************************************************************************
+ 
+ *  Purpose: perform compounded interest  
+ *  monthly
+ *
+ *  @author  PriyankaTimgol
+ *  @version 1.0
+ *  @since   09-08-2017
+ *
+ ******************************************************************************/package com.bridgelabz.javaBasicprograms;
+
+import com.bridgelabz.Utility.Utility;
+
+public class CarLoan {
+
+	public static void main(String[] args) {
+		double principle=Integer.parseInt(args[0]);  
+		double year=Integer.parseInt(args[1]);
+		double Rate=Integer.parseInt(args[2]);
+		System.out.println("principle: "+principle+"\nyear: "+year+"\nrate: "+Rate);
+		double result=Utility.carloan(principle,year,Rate);
+		
+		System.out.println("Monthly compound interest: "+result);
+	}
+
+}
