f6e30db102d4e6501444a34c75df26ae69508379
satyajitpanda94 <saatjit94@gmail.com>
2019-08-14 13:32:03 +0530

    java functional programs

--- src/com/bridgelabz/Utility/Utility.java
+++ src/com/bridgelabz/Utility/Utility.java
@@ -362,6 +362,17 @@ public class Utility {
 					}
 				}
 
+					public static void primefactor(int number) {
+						for(int i=2;i<number;i++) {
+							while(number%i==0) {
+								System.out.println(i+" ");
+								number=number/i;
+							}
+						}
+						if(number>2) {
+							System.out.println(number);
+						}
+					}
 }
 				
 				
