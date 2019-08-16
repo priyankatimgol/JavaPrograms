cfcd47b0c08ba5225615446701dc0b34af6cdbb6
priyankatimgol <48207687+priyankatimgol@users.noreply.github.com>
2019-08-16 11:33:05 +0530

    Update Utility.java

--- src/com/bridgelabz/Utility/Utility.java
+++ src/com/bridgelabz/Utility/Utility.java
@@ -315,69 +315,6 @@ public static void binary(int number) {
 }
     
 
-
-			
-//
-//			/**
-//			 * @param temperature Enter temperature is larger than 50 
-//			 * @param speed Enter speed  is larger than 120 or less than 3
-//			 * @return wind value
-//			 */
-//			public static double windchill(double temperature,double speed) {
-//				
-//				double wind=35.74+0.6215*temperature+(0.4275*temperature-35.75)*Math.pow(0,16);
-//				return wind;
-//			} 
-//			
-//			/**
-//			 * @param day day added
-//			 * @param month month added
-//			 * @param year year added
-//			 * @return day
-//			 * 
-//			 */
-//			public static int dayofweek(int day,int month,int year) {
-//				
-//				int year1=year-(14-month)/12;
-//				int year2= year1 + year1/4 - year1/100 + year1/400;
-//				int month1 = month + 12 *((14 - month ) / 12)- 2;
-//				int day1= (day + year2 + 31 *month1/12)% 7;
-//				return day1;
-//				
-//			}
-//			/**
-//			 * @param principle principle added
-//			 * @param year year added
-//			 * @param Rate Rate added
-//			 * @return monthly compound interest
-//			 */
-//			public static double  carloan(double principle,double year,double Rate) {
-//				
-//				double number=12*year;
-//				double rate=Rate/(12*100);
-//			    double payment=(principle*rate)/(1-(Math.pow((1+rate),-number)));
-//				return payment;
-//				
-//			}
-//			/**
-//			 * @param angle Entered angle in radians
-//			 */
-//			public static void trig(double angle) {
-//				
-//				double sin=Math.sin(angle);
-//				System.out.println("angle in sin: "+sin);
-//				double cos=Math.cos(angle);
-//				System.out.println("angle in cos: "+cos);
-//				double tan=Math.tan(angle);
-//				System.out.println("angle in tan: "+tan);
-//				double cot=1/tan;
-//				System.out.println("angle in cot: "+cot);
-//				double cosec=1/sin;
-//				System.out.println("angle in cosec: "+cosec);
-//				double sec=1/cos;
-//				System.out.println("angle in sec: "+sec);
-//				}
-//			
 			/*************** Condition, Loops and Logical Programs *****************/
 			
 			/**
